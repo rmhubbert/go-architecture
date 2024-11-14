4fcaaf5b4ed09358e81ab5abb24bdf003d568ce3
Hubby <hubby@rmhubbert.com>
2024-11-14 14:56:11 +0000

    feat: add correct update and delete routes for user

--- simple/cmd/simple/routes.go
+++ simple/cmd/simple/routes.go
@@ -10,6 +10,8 @@ func initRoutes(router *http.ServeMux, userHandler *app.UserHandler) {
 	router.HandleFunc("GET /user/{id}", userHandler.GetOne)
 	router.HandleFunc("GET /users", userHandler.GetMany)
 	router.HandleFunc("POST /user", userHandler.Create)
-	router.HandleFunc("PUT /user", userHandler.Update)
-	router.HandleFunc("DELETE /user", userHandler.Delete)
+	router.HandleFunc("PATCH /user/{id}", userHandler.Update)
+	router.HandleFunc("DELETE /user/{id}", userHandler.Delete)
+
+	router.HandleFunc("PATCH /user/password/{id}", userHandler.UpdatePassword)
 }
