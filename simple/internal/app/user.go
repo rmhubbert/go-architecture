eee58988ab242181d76043a263547bec266ff852
Hubby <hubby@rmhubbert.com>
2024-11-14 14:56:35 +0000

    feat: add user DTOs

--- simple/internal/app/user.go
+++ simple/internal/app/user.go
@@ -1,8 +1,62 @@
 package app
 
 type User struct {
-	Id       int    `json:"id"`
+	Id       int
+	Name     string
+	Email    string
+	Password string
+}
+
+type CreateUserInput struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password,omitempty"`
+}
+
+func (cu *CreateUserInput) User() *User {
+	return &User{
+		Name:     cu.Name,
+		Email:    cu.Email,
+		Password: cu.Password,
+	}
+}
+
+type UpdateUserInput struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func (cu *UpdateUserInput) User() *User {
+	return &User{
+		Id:    cu.Id,
+		Name:  cu.Name,
+		Email: cu.Email,
+	}
+}
+
+type UserOutput struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func NewUserOutput(user *User) *UserOutput {
+	return &UserOutput{
+		Id:    user.Id,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+}
+
+type UpdateUserPasswordInput struct {
+	Id       int    `json:"id"`
+	Password string `json:"password"`
+}
+
+func (pu *UpdateUserPasswordInput) User() *User {
+	return &User{
+		Id:       pu.Id,
+		Password: pu.Password,
+	}
 }
